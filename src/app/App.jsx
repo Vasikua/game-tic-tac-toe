616d64eef5ac04e3749beeb82ec1f0dfcde44772
Vasyl Mandik <Vasik_ua@ukr.net>
2025-02-05 23:46:07 -0600

    final

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Cell from "../cell/Cell";
+
 const App = () => {
-  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
-  console.log("all work good");
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [go, setGo] = useState("circle");
+  const [winningMessage, setWinningMassage] = useState(null);
+
+  const message = "Next move: " + go;
+
+  const handleReset = () => {
+    setCells(Array(9).fill(""));
+    setWinningMassage(null);
+  };
+
+  useEffect(() => {
+    const winningCombo = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winningCombo.forEach(arr => {
+      let crossWin = arr.every(cell => cells[cell] === "cross");
+      if (crossWin) {
+        setWinningMassage("cross win!!!");
+        return;
+      }
+    });
+    winningCombo.forEach(arr => {
+      let circleWin = arr.every(cell => cells[cell] === "circle");
+      if (circleWin) {
+        setWinningMassage("circle win!!!");
+        return;
+      }
+    });
+  }, [cells]);
   return (
     <div className="tic-tac-toe">
+      <h1>Tic-Tac-Toe</h1>
       <div className="gameBoard">
         {cells.map((cell, index) => (
-          <Cell key={index} id={index} cell={cell} />
+          <Cell
+            key={index}
+            id={index}
+            cells={cells}
+            setCells={setCells}
+            go={go}
+            setGo={setGo}
+            winningMessage={winningMessage}
+          />
         ))}
       </div>
-      <p></p>
+      <p>{winningMessage || message}</p>
+      <button className="btnRes" onClick={handleReset}>
+        reset
+      </button>
     </div>
   );
 };
--- src/cell/Cell.jsx
+++ src/cell/Cell.jsx
@@ -1,8 +1,25 @@
 import "./Cell.css";
-const Cell = ({ cell, id, index }) => {
+import { FaRegCircle } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
+const Cell = ({ cells, id, go, setGo, setCells, winningMessage }) => {
+  const handleClick = e => {
+    if (winningMessage) {
+      return;
+    }
+    if (cells[id]) {
+      return;
+    }
+    const nextCells = cells.map((cell, index) => (index === id ? go : cell));
+    setCells(nextCells);
+    setGo(go === "circle" ? "cross" : "circle");
+  };
+
+  console.log(cells);
   return (
-    <div className="square" id={id}>
-      <p>{index}</p>
+    <div className="square" id={id} onClick={handleClick}>
+      {cells[id] === "circle" && <FaRegCircle size={60} color="blue" />}
+      {cells[id] === "cross" && <ImCross size={60} color="red" />}
     </div>
   );
 };

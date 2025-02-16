1846ceea56e0aafea0efaa7124d7f12ea5580f62
Vasyl Mandik <Vasik_ua@ukr.net>
2025-02-16 16:23:18 -0600

    fix message

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,28 +4,31 @@ import Cell from "../cell/Cell";
 
 const App = () => {
   const [cells, setCells] = useState(Array(9).fill(""));
-  const [go, setGo] = useState("circle");
+  const [go, setGo] = useState("cross");
   const [winningMessage, setWinningMassage] = useState(null);
 
-  const message = "Next move: " + go;
+  const message =
+    cells.every(cell => cell != "") && !winningMessage
+      ? "game end no one winns "
+      : "Next move: " + go;
 
   const handleReset = () => {
     setCells(Array(9).fill(""));
     setWinningMassage(null);
   };
 
-  useEffect(() => {
-    const winningCombo = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  const winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
+  useEffect(() => {
     winningCombo.forEach(arr => {
       let crossWin = arr.every(cell => cells[cell] === "cross");
       if (crossWin) {
@@ -41,6 +44,7 @@ const App = () => {
       }
     });
   }, [cells]);
+
   return (
     <div className="tic-tac-toe">
       <h1>Tic-Tac-Toe</h1>
@@ -59,7 +63,7 @@ const App = () => {
       </div>
       <p>{winningMessage || message}</p>
       <button className="btnRes" onClick={handleReset}>
-        reset
+        RESET
       </button>
     </div>
   );
